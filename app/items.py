eea06ede38d02ddf6eb7609f4f00c77eab5aabdc
nmaslov <nmaslov255@yandex.ru>
2022-03-22 19:15:35 +0300

    added ProductItem in items.py

--- app/items.py
+++ app/items.py
@@ -6,7 +6,7 @@
 import scrapy
 
 
-class AppItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class ProductItem(scrapy.Item):
+    category = scrapy.Field()
+    name = scrapy.Field()
+    price = scrapy.Field()
